bc0a65494c6b8f2514f77964588baadad9f3fcab
Ravi Gupta <ravigupta82974@gmail.com>
2022-05-21 00:37:05 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  
+  const d = new Date();
+
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let month = months[d.getMonth()];
+
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let day = days[d.getDay()];
+
+  let date = d.getDate();
+  let year = d.getFullYear();
+
+  let time = new Date().toLocaleTimeString();
+  const [ctime,setCtime] = useState(time);
+  
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+
+  setInterval(UpdateTime,1000);
+
+  return(
+    <div className='App-header'>
+      <h1>{date}&ensp;{month}&ensp;{year}</h1>
+      <h1>{day}</h1>
+      <h1>{ctime}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
